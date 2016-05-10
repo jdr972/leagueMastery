c63e75f27234ba601b57cb5bda70b313ab6e9a7e
Jacques de Reynal <jacques@dereynal.com>
2016-05-10 18:27:15 +0200

    xd

---
+++ index.js
@@ -0,0 +1,161 @@
+var express = require('express')
+var app = express()
+var request = require('request')
+var path = require('path')
+var apiKey = "5eb57e94-25e1-43d1-8d45-84423c2e7c0b"
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+
+app.use(allowCrossDomain)
+app.use('/', express.static('public'));
+
+
+app.get('/api/summonerinfo', function(req, res) {
+    var name = req.query.name
+    console.log("info for " + name + " requested.")
+    var url = "https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/" + name + "?api_key="+apiKey
+    request({
+        url: url,
+        json: true
+    }, function(error, response, data) {
+        if (!error) {
+            console.log("No error encountered")
+            res.end(JSON.stringify(data))
+        } else {
+            console.log(error)
+        }
+    })
+})
+
+
+app.get('/api/champinfo', function(req, res) {
+    var id = req.query.id
+    console.log("info for id " + id + " requested.")
+    var nameUrl = "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/" + id + "?api_key="+apiKey;
+    request({
+        url: nameUrl,
+        json: true
+    }, function(error, response, data) {
+        var champName = data.name
+        var infoUrl = "https://ddragon.leagueoflegends.com/cdn/6.9.1/data/en_US/champion/" + champName + ".json";
+        request({
+            url: infoUrl,
+            json: true
+        }, function(error, response, data) {
+            if (!error) {
+                console.log("No error encountered")
+                res.end(JSON.stringify(data))
+            } else {
+                console.log(error)
+            }
+
+        })
+    })
+})
+
+app.get('/api/champname', function(req, res) {
+    var id = req.query.id
+    console.log("name for id " + id + " requested.")
+    var nameUrl = "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/" + id + "?api_key="+apiKey
+    request({
+        url: nameUrl,
+        json: true
+    }, function(error, response, data) {
+        if (!error) {
+            console.log("No error encountered")
+            res.end(data.name)
+        } else {
+            console.log(error)
+        }
+    })
+})
+
+app.get('/api/champmastery', function(req, res) {
+    var name = req.query.name
+    console.log("mastery info for " + name + " requested.")
+    var url = "https://server-jdr972.c9users.io/api/summonerinfo?name="+name
+    request({
+        url: url,
+        json: true
+    }, function(error, response, data) {
+        if (data) {
+            console.log("No error encountered")
+            var summonerId = data[name].id
+            console.log("Summmoner id : " + JSON.stringify(summonerId))
+            var reqUrl = "https://euw.api.pvp.net/championmastery/location/euw1/player/" + summonerId + "/champions?api_key="+apiKey
+            request({
+                url: reqUrl,
+                json: true
+            }, function(error, response, data) {
+                if (!error) {
+                    console.log("No error encountered")
+                    //console.log(response)
+                    res.end(JSON.stringify(data))
+                } else {
+                    console.log(error)
+                }
+            })
+
+        } else {
+            console.log(error)
+        }
+    })
+})
+
+app.get('/api/ranked', function(req, res) {
+    var name = req.query.name
+    console.log("ranked info for " + name + " requested.")
+    var url = "https://server-jdr972.c9users.io/api/summonerinfo?name="+name
+    request({
+        url: url,
+        json: true
+    }, function(error, response, data) {
+        if (data && !data.status) {
+            console.log("No error encountered")
+            var summonerId = data[name].id
+            console.log("Summmoner id : " + JSON.stringify(summonerId))
+            var reqUrl = "https://euw.api.pvp.net/api/lol/euw/v2.5/league/by-summoner/"+summonerId+"/entry?api_key="+apiKey
+            request({
+                url: reqUrl,
+                json: true
+            }, function(error, response, data) {
+                if (!error) {
+                    console.log("No error encountered")
+                    //console.log(response)
+                    res.end(JSON.stringify(data))
+                } else {
+                    console.log(error)
+                }
+            })
+
+        } else {
+            console.log(error)
+        }
+    })
+})
+
+app.get('/api/champskins', function(req, res) {
+    var id = req.query.id
+    console.log("skins for champion " + id + " requested.")
+    var url = "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+id+"?champData=skins&api_key="+apiKey
+    request({
+        url: url,
+        json: true
+    }, function(error, response, data) {
+        if (data) {
+            console.log("No error encountered")
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify(data))
+
+        } else {
+            console.log(error)
+        }
+    })
+})
+
+app.listen(8080)
---
+++ public/script.js
@@ -0,0 +1,188 @@
+function main(){
+  $("#masteryData").fadeOut(1)
+  $(".btn").click(function(){
+    var summonerName = document.getElementById('input').value;
+    var name = summonerName.split(' ').join("")
+    name = name.toLowerCase();
+    fetchInfo(name)
+  })
+}
+var count = 0;
+var masteryData;
+var summonerId;
+var tier= "", division ="", leaguePoints= "", wins ="", losses ="";
+var summonerName = undefined;
+var level = undefined;
+var icon = undefined;
+var iconUrl = undefined;
+var summonerId = undefined;
+var champions = getChamps()
+
+
+function getChamps(){
+  var coolData
+  $.ajax({
+    url: "https://server-jdr972.c9users.io/champions.json",
+    success: function(data){coolData = data},
+    async: false
+  })
+  return coolData
+}
+
+function fetchInfo(sumName){
+  var url = "https://server-jdr972.c9users.io/api/summonerinfo?name="+sumName
+  $.getJSON(url, function(object){
+    summonerId = object[sumName].id;
+    summonerName = object[sumName].name;
+    console.log(summonerName)
+    level = object[sumName].summonerLevel;
+    icon = object[sumName].profileIconId;
+    iconUrl = "https://ddragon.leagueoflegends.com/cdn/6.8.1/img/profileicon/"+icon+".png"
+    var rankedUrl = "https://server-jdr972.c9users.io/api/ranked?name="+sumName
+    $.getJSON(rankedUrl, function(object){
+      if(!object.hasOwnProperty('status')){
+        console.log(object[summonerId])
+          tier = object[summonerId][0].tier
+          console.log(tier)
+          $.each(object[summonerId][0].entries[0], function(key, value){
+            if(key == "division"){division = value}
+            if(key == "leaguePoints"){leaguePoints = value}
+            if(key == "wins"){wins = value}
+            if(key == "losses"){losses = value}
+          })
+
+        }
+        else{
+          tier= ""
+          division =""
+           leaguePoints= ""
+            wins =""
+            losses =""
+        }
+        displayInfo()
+      })
+})
+}
+
+function displayInfo(){
+  var rank = "", lp = "", winlos ="", sumLink = "";
+  $(".basic").fadeOut(600, function(){
+    $("#profileimg").attr("src", iconUrl)
+    $("#profileimg").attr("height", "128px")
+    sumLink = "<a href='http://euw.op.gg/summoner/userName="+summonerName+"'>"+summonerName+"</a>"
+    $('#sumName').html(sumLink)
+    if(tier !== ""){
+      rank = tier + " " + division
+      $("#rank").html(rank)
+      lp = leaguePoints + " LP"
+      $("#lp").html(lp)
+      winlos = wins+"W/"+losses+"L"
+      $("#winlos").html(winlos)
+    }
+    else{
+      $("#rank").html('Unranked')
+      $("#lp").html('')
+      $("#winlos").html('')
+    }
+    $(".basic").css("padding-top", "15px")
+    $(".basic").css("padding-bottom", "15px")
+    $(".basic").fadeIn(600, function(){
+      displayMastery()
+    })
+
+  })
+}
+
+
+function displayMastery(){
+  var sumName = document.getElementById('input').value;
+  var name = summonerName.split(' ').join("")
+  name = name.toLowerCase();
+  masteryData = undefined;
+  masteryData = getMasteryData(name);
+  console.log(masteryData.length)
+  var list1 = '';
+  var list2 = '';
+  var list3 = '';
+  var list4 = '';
+  $("#masteryData").fadeOut(500, function(){
+    for(var i = 0; i<masteryData.length; i++){
+      $.each(champions.data, function(key, value){
+        $.each(champions.data[key], function(k, v){
+          if(k == "key" && v == masteryData[i].championId){
+            if(i == 0){
+              var skinNumber = 0;
+              var url = "https://server-jdr972.c9users.io/api/champskins?id="+masteryData[i].championId
+              $.ajax({
+                url: url,
+                async: false,
+                //dataType: 'json',
+                success: function(data){skinNumber = data.skins.length}
+              })
+              var number = Math.floor(Math.random() * skinNumber)
+              if(number){
+                changeBg("https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+key+"_"+number+".jpg")
+              }
+              else{
+                changeBg("https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+key+"_0.jpg")
+
+                }
+            }
+            list1 += `<img src="https://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/`+key+`.png" alt="" height="49.5px"/>`
+            $("#list1").html(list1)
+            list2 += `<p>`+key+`</p>`
+            $("#list2").html(list2)
+            list3 += `<p>Level `+masteryData[i].championLevel+`</p>`
+            $("#list3").html(list3)
+            var spacedPoints = masteryData[i].championPoints
+            spacedPoints = spacedPoints.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+            list4 += `<p>`+spacedPoints+`</p>`
+            $("#list4").html(list4)
+            $("#masteryData").fadeIn(1000)
+          }
+        })
+      })
+    }
+  })
+}
+
+function getMasteryData(dude){
+  var url = "https://server-jdr972.c9users.io/api/champmastery?name="+dude
+  var someData
+  $.ajax({
+    url: url,
+    dataType: "json",
+    success:  function(data){someData = data},
+    async: false
+  })
+  return someData;
+}
+
+function changeBg(src){
+  var img = new Image()
+  img.onload = function(){
+    $("body").css("background", '#1C1919 url("'+src+'") no-repeat right top')
+    $("body").css("background-attachment", "fixed")
+    $("body").css("background-size", "cover")
+  }
+  img.src = src
+}
+
+var defaultBg = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Vayne_0.jpg";
+$.ajax({
+  url: "https://server-jdr972.c9users.io/api/champskins?id=67",
+  success: function(data){
+    var number = Math.floor(Math.random() * data.skins.length)
+    if(number){
+      changeBg("https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Vayne_"+number+".jpg")
+    }
+    else{
+      changeBg(defaultBg)
+    }
+  }
+})
+
+
+
+
+$(document).ready(main)
